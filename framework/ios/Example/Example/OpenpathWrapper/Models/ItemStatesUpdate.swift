948b01e55c4c531025615dee3daa7582935eb612
elpsar-erin <elang.sartika@erinliving.com>
2025-08-07 10:22:32 +0700

    upgrade

--- framework/ios/Example/Example/OpenpathWrapper/Models/ItemStatesUpdate.swift
+++ framework/ios/Example/Example/OpenpathWrapper/Models/ItemStatesUpdate.swift
@@ -1,12 +1,12 @@
 struct ItemState: Decodable {
-    let isScheduledRemoteUnlockAllowed: Bool
-    let isScheduledAvailableRevert: Bool
+    let isScheduledRemoteUnlockAllowed: Bool?
+    let isScheduledAvailableRevert: Bool?
     let itemType: ItemType
     let itemId: Int
-    let isScheduledNoAccess: Bool
-    let isScheduledTouchAllowed: Bool
-    let isScheduledUnlocked: Bool
-    let isScheduledOverrideAllowed: Bool
+    let isScheduledNoAccess: Bool?
+    let isScheduledTouchAllowed: Bool?
+    let isScheduledUnlocked: Bool?
+    let isScheduledOverrideAllowed: Bool?
 }
 
 struct ItemStatesUpdate: Decodable {
